af29087a336d198bbd9438399f9097ac0a7cb4d0
Benjamin Sengupta <benjaminsengupta@gmail.com>
2024-03-23 16:30:23 -0400

    Try SameSite=None

--- tucon_backend/middlewares/flask_session.py
+++ tucon_backend/middlewares/flask_session.py
@@ -14,7 +14,7 @@ redis = Redis(
 app.config.update(
     SESSION_COOKIE_SECURE=True,
     SESSION_COOKIE_HTTPONLY=True,
-    SESSION_COOKIE_SAMESITE="Lax",
+    SESSION_COOKIE_SAMESITE="None",
 )
 app.secret_key = config.FLASK_SECRET_KEY
 app.session_interface = RedisSessionInterface(app=app, client=redis)
