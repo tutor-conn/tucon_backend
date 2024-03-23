4c62b7f5b9468f01d5dbec380f5463192a61f853
Benjamin Sengupta <benjaminsengupta@gmail.com>
2024-03-23 15:56:02 -0400

    Remove CORS origins

--- tucon_backend/middlewares/cors.py
+++ tucon_backend/middlewares/cors.py
@@ -1,7 +1,4 @@
 from tucon_backend import app
-from tucon_backend.config import get_config
 from flask_cors import CORS
 
-config = get_config()
-
-CORS(app, origins=config.CORS_ORIGIN, supports_credentials=True)
+CORS(app, supports_credentials=True)
